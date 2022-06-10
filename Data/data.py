62f14ef47025adeb1041cd67669e5c20f03bd6e3
Larissa Carvalho <dev.larissacarvalho@gmail.com>
2022-06-10 14:25:53 -0300

    refatoracao da classe Data

--- Data/data.py
+++ Data/data.py
@@ -20,12 +20,25 @@ class Data:
             self.__mes = 0
             self.__ano = 0
 
-    @staticmethod
-    def getDateToday():
-        data = date.today()
-        print(data.strftime("%d/%m/%Y"))
+    # Métodos get e set por decorator. Modo pythonic
+    @property
+    def data(self):
+        return (self.__dia, self.__mes, self.__ano)
 
-    def mudarDataPara(self, dia = 0, mes = 0, ano = 0, outraData = None, qtdDias = 0):
+    @property
+    def dia(self):
+        return self.__dia
+
+    @property
+    def mes(self):
+        return self.__mes
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @data.setter
+    def data(self, dia = 0, mes = 0, ano = 0, outraData = None, qtdDias = 0):
         if(outraData != None):
             self.__dia = outraData.__dia
             self.__mes = outraData.__mes
@@ -38,9 +51,32 @@ class Data:
             self.__dia = dia
             self.__mes = mes
             self.__ano = ano
+
+    @dia.setter
+    def dia(self, dia):
+        self.__dia = dia
+
+        if(not self.__verificarData(self)):
+            self.__dia = 0
+
+    @mes.setter
+    def mes(self, mes):
+        self.__mes = mes
+
+        if(not self.__verificarData(self)):
+            self.__mes = 0
+
+    @ano.setter
+    def ano(self, ano):
+        self.__ano = ano
+
+        if(not self.__verificarData(self)):
+            self.__ano = 0
+
     
+    # Métodos privados
     def __adiantaDias(self, adianta):
-        qtdDias = self.__diaMes
+        qtdDias = self.__quantosDiasNoMes
 
         for _ in range(adianta + 1):
             if(self.__dia == qtdDias):
@@ -59,127 +95,19 @@ class Data:
 
         return True
 
-    def formatData(self, data):
-        [dia, mes, ano] = data.split("/")
-        
-        dia = int(dia)
-        mes = int(mes)
-        ano = int(ano)
-
-        self.setData(dia, mes, ano)
-
-    def dateString(self):
-        mes = ("janeiro", "feveiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", 
-               "outubro", "novembro", "dezembro")
-
-        date = str(self.__dia) + " de " + str(mes[self.__mes - 1]) + " de " + str(self.__ano)
-
-        return date
-
-    def setData(self, dia, mes, ano):
-        self.__dia = dia
-        self.__mes = mes
-        self.__ano = ano
-
-        if(not self.__verificarData()):
-            self.__dia = 0
-            self.__mes = 0
-            self.__ano = 0
-            self.__data = str(00) + "/" + str(00) + "/" + str(00)
-
-    def setDia(self, dia):
-        self.__dia = dia
-
-        if(not self.__verificarData(self)):
-            self.__dia = 0
-
-    def setMes(self, mes):
-        self.__mes = mes
-
-        if(not self.__verificarData(self)):
-            self.__mes = 0
-
-    def setAno(self, ano):
-        self.__ano = ano
-
-        if(not self.__verificarData(self)):
-            self.__ano = 0
-
-    def getData(self):
-        return self.__data
-
-    def getDia(self):
-        return self.__dia
-
-    def getMes(self):
-        return self.__mes
-
-    def getAno(self):
-        return self.__ano
-
-    def eAnterior(self, data):
-        if(self.quantosDiasAte(data) < 0):
-            return True
-        
-        return False
-
-    def quantosDiasAte(self, data):
-        [outroDia, outroMes, outroAno] = data.split('/')
-        totalDia = 0
-
-        dia = self.getDia()
-        mes = self.getMes()
-        ano = self.getAno()
-
-        outroDia = int(outroDia)
-        outroMes = int(outroMes)
-        outroAno = int(outroAno)
-
-        if(ano <= outroAno):
-            totalDia += outroDia - dia
-    
-            for n in range(ano, outroAno):
-                totalDia += self.__diaAno(n)
-            
-            if(mes < outroMes):
-                for n in range(mes, outroMes):
-                    totalDia += self.__diaMes(n, ano)
-
-            elif(mes > outroMes):
-                for n in range(outroMes, mes):
-                    totalDia -= self.__diaMes(n, ano)
-
-            return totalDia
-
-        else:
-            totalDia += dia - outroDia
-
-            for n in range(outroAno, ano):
-                totalDia += self.__diaAno(n)
-
-            if(outroMes < mes):
-                for n in range(outroMes, mes):
-                    totalDia += self.__diaMes(n, outroAno)
-
-            elif(outroMes > mes):
-                for n in range(mes, outroMes):
-                    totalDia -= self.__diaMes(n, outroAno)
-
-            return -totalDia
-
     def __eBissexto(self, ano):
         if(ano % 4 == 0):
             return True
 
         return False
 
-    def __diaAno(self, ano):
+    def __quantosDiasNoAno(self, ano):
         if(self.__eBissexto(ano)):
             return 366
         else:
             return 365
 
-    def __diaMes(self, mes, ano):
+    def __quantosDiasNoMes(self, mes, ano):
         if((mes == 2) and (self.__eBissexto(ano))):
             return 29
 
@@ -192,6 +120,8 @@ class Data:
         else: 
             return 30
 
+    
+    # Métodos públicos
     def qualDiaDaSemana(self):
         semana = ("sábado", "domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", 
                   "sexta-feira")
@@ -207,3 +137,67 @@ class Data:
         diaDaSemana = floor(aux % 7)
 
         return semana[diaDaSemana]
+
+    def toString(self):
+        mes = ("janeiro", "feveiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", 
+               "outubro", "novembro", "dezembro")
+
+        date = str(self.__dia) + " de " + str(mes[self.__mes - 1]) + " de " + str(self.__ano)
+
+        return date
+
+    def eAnterior(self, other):
+        if(self.quantosDiasAte(other) < 0):
+            return True
+        
+        return False
+
+    def quantosDiasAte(self, outraData):
+        
+        totalDia = 0
+
+        dia = self.dia()
+        mes = self.mes()
+        ano = self.ano()
+
+        outroDia = outraData.dia()
+        outroMes = outraData.getMes()
+        outroAno = outraData.getAno()
+
+        if(ano <= outroAno):
+            totalDia += outroDia - dia
+    
+            for n in range(ano, outroAno):
+                totalDia += self.__quantosDiasNoAno(n)
+            
+            if(mes < outroMes):
+                for n in range(mes, outroMes):
+                    totalDia += self.__quantosDiasNoMes(n, ano)
+
+            elif(mes > outroMes):
+                for n in range(outroMes, mes):
+                    totalDia -= self.__quantosDiasNoMes(n, ano)
+
+            return totalDia
+
+        else:
+            totalDia += dia - outroDia
+
+            for n in range(outroAno, ano):
+                totalDia += self.__quantosDiasNoAno(n)
+
+            if(outroMes < mes):
+                for n in range(outroMes, mes):
+                    totalDia += self.__quantosDiasNoMes(n, outroAno)
+
+            elif(outroMes > mes):
+                for n in range(mes, outroMes):
+                    totalDia -= self.__quantosDiasNoMes(n, outroAno)
+
+            return -totalDia
+
+    # Métodos estáticos   
+    @staticmethod
+    def getDateToday():
+        data = date.today()
+        print(data.strftime("%d/%m/%Y"))
